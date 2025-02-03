7e232645e68ee4b6f52e346950ff8d6e8813e182
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-02 22:53:16 -0500

    Created journal.js
    Created the barebones of journal.js, need to keep updating this one

---
+++ app/(tabs)/journal.js
@@ -0,0 +1,20 @@
+/* Ofe */
+/* Journal Page [journal.js] */
+
+/*By Jonathan Bruce
+JBruce, LLC
+jbruce.design
+.j.
+- - - - - */
+
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import React from "react";
+
+export default function JournalScreen() {
+    return (
+        <View>
+            <Text>Journal</Text>
+            <TextInput></TextInput>
+        </View>
+    );
+}
