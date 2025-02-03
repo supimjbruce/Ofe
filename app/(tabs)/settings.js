3a4876245cf87387a282fd30a03137470c311b6f
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-02 22:53:32 -0500

    Created settings.js
    Created the barebones of settings.js, need to keep working on this one

---
+++ app/(tabs)/settings.js
@@ -0,0 +1,19 @@
+/* Ofe */
+/* Settings Page [settings.js] */
+
+/*By Jonathan Bruce
+JBruce, LLC
+jbruce.design
+.j.
+- - - - - */
+
+import {View, Text, StyleSheet} from 'react-native';
+import React from "react";
+
+export default function SettingsScreen() {
+    return (
+        <View>
+            <Text>Settings</Text>
+        </View>
+    );
+}
