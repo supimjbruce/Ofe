6fcccbe6ca84263c37b8f9c0de55070c893f4ed2
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-02 21:56:56 -0500

    Updated index.js
    Changed the background color of the Home page to be like the Figma design
    
    .j.

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#C7B198',
   },
   title: {
     fontFamily: 'PirataOne-Regular',
