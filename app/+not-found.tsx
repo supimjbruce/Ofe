e1286e84449a0d6f920c9dc8b08c7171882152c3
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-14 09:52:41 -0500

    Updated +not-found.tsx
    We're back GitHub! Got my Macbook back from repair yesterday and today we back to coding!
    
    Today I wanted to fix the not found a little bit since I accidentally went to it. Took out the top bar (make it more smooth), added in text breaks (make this cleaner) and tried to add in better styling for the bottom button.
    
    Going to keep working on this page
    
    Thank you for reading!
    .j.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -14,15 +14,23 @@ import {Stack, Link} from 'expo-router';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{title: 'Page Not Found'}} />
+      <Stack.Screen options={{
+        headerShown: false,
+        title: 'Page Not Found'
+        }}
+      />
       <View style={styles.container}>
         <Text style={styles.text}>
             We could not find the page you were looking for!
+            <br></br>
             We are extremely sorry! Click the button below to go back to the home page.
+            <br></br>
 
             Thank you and have a blessed day!
-
+            <br></br>
+            <br></br>
             -JBruce
+            <br></br>
             Creator of Ofe
         </Text>
         <Link href="/" style={styles.button}>
@@ -36,26 +44,25 @@ export default function NotFoundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0ECE2',
     justifyContent: 'center',
     alignItems: 'center',
   },
 
   text: {
     flex: 1,
+    alignContent: 'center',
     textAlign: 'center',
     fontSize: 16,
     color: '#000'
   },
 
   button: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    color: '#fff',
-    fontSize: 20,
-    borderRadius: 10,
-    borderColor: '#000',
+    color: '#000',
+    fontSize: 16,
+    /*borderRadius: 10,
+    borderColor: '#000',*/ /*<--- I want to style this like a button, figure this out */
+    paddingBottom: 20,
   },
 });
 
