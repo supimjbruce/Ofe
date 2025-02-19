4087a39929b9b4a69d3b2467c14c53d2ab2de964
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-19 15:28:27 -0500

    Updated journal.js
    Added in the CreateJournalPost Component as well as a Flatlist that will eventually house the journal posts (commented out)
    
    .j.

--- app/(tabs)/journal.js
+++ app/(tabs)/journal.js
@@ -7,14 +7,37 @@ jbruce.design
 .j.
 - - - - - */
 
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet, FlatList} from 'react-native';
 import React from "react";
+import CreateJournalPost from '../../components/createJournalPostModal';
 
 export default function JournalScreen() {
     return (
-        <View>
-            <Text>Journal</Text>
-            <TextInput></TextInput>
+        <View style={styles.container}>
+            <CreateJournalPost></CreateJournalPost>
+            {/*<View>
+                <FlatList
+                    data={[
+                    {key: 'Creation'},
+                    {key: 'Fall'},
+                    {key: 'Jesus Coming to Earth'},
+                    {key: 'The Cross'},
+                    {key: 'Three Days Later'},
+                    {key: 'Ascension'},
+                    {key: 'His Return'},
+                    {key: 'What does this mean for you?'},
+                    ]}
+                    renderItem={({item}) => <Text style={styles.listItem}>{item.key}</Text>}
+                />
+            </View> <- Work on creating a way to show data in a list like iOS, for journal posts.*/}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#F0ECE2',
+    },
+});
