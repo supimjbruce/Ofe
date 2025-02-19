214f84fe8cf61b41a0d71cf6f401cdf202c4f5f4
Jonathan Bruce <supimjbruce@gmail.com>
2025-02-19 15:27:28 -0500

    Created createJournalPostModal.js
    Started this over my lunch break and then finished it. Super basic building block code that does nothing but needed to make a button that can create a journal post with the style of the Figma design
    
    .j.

---
+++ components/createJournalPostModal.js
@@ -0,0 +1,29 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from "react";
+
+export default function CreateJournalPost() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.buttonTitle}>+</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#295F4E',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    margin: 20,
+  },
+
+  buttonTitle: {
+    fontSize: 32,
+    color: 'white',
+    textAlign: 'center',
+    fontFamily: 'Poppins-Bold',
+  }
+});
